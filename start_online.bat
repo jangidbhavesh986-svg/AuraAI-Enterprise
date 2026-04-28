839e6df3d971314323bdfa0e225b9c7c15793219
jangidbhavesh986-svg <jangidbhavesh986@gmail.com>
2026-04-28 16:24:35 +0530

    Create start_online.bat

---
+++ start_online.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Starting AuraAI Local Server...
+start "AuraAI Server" /min python -m http.server 5000
+echo.
+echo Connecting to Public Tunnel (localhost.run)...
+echo.
+echo Copy the HTTPS URL from the output below:
+echo.
+ssh -o StrictHostKeyChecking=no -R 80:localhost:5000 nokey@localhost.run
